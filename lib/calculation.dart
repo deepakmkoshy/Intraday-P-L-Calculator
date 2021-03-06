7eaf2ddf2b3d5cdda6adfeedaa10a709199ab3dc
Deepak <deepakmkoshy@gmail.com>
2021-03-06 17:02:47 +0530

    Migrated to dart null safety

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,18 +1,22 @@
+
+
+
 class Calculation {
-  double profit;
+  late double profit;
   double buy;
   double sell;
-  double totalAmount;
-  double brokerage;
-  double stt;
-  double gst;
-  double turnoverCharge;
-  double gstTurnover;
-  double totalFees;
-  String profitper;
+  late double totalAmount;
+  late double brokerage;
+  late double stt;
+  late double gst;
+  late double turnoverCharge;
+  late double gstTurnover;
+  late double totalFees;
+  String? profitper;
   double totalProfit = 0;
 
-  Calculation({this.buy, this.sell});
+
+  Calculation({required this.buy, required this.sell});
   void calculate() {
     profit = sell - buy;
     totalAmount = sell + buy;
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:intradaypl/calculation.dart';
 
 class Homepage extends StatefulWidget {
-  Homepage({Key key}) : super(key: key);
-
   @override
   _HomepageState createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
+//  TabController _tabController;
+
   final _buyController = TextEditingController();
   final _sellController = TextEditingController();
-  double width;
-  double height;
-  Calculation calculation = Calculation();
+  late double width;
+  late double height;
+  late Calculation calculation = Calculation(buy: 0, sell: 0);
   bool isCalActive = false;
   bool _showBuyValidationError = false;
   bool _showSellValidationError = false;
@@ -98,74 +98,84 @@ class _HomepageState extends State<Homepage> {
         backgroundColor: Color(0XFF0043b4),
       ),
       backgroundColor: Colors.grey[100],
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    textField('Buy Amount', _buyController),
-                    SizedBox(
-                      width: width * 0.05,
-                    ),
-                    textField('Sell Amount', _sellController),
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  primary: Color(0XFF0043b4),
-                ),
-                onPressed: () {
-                  //Implement null check both input fields
-                  print(_buyController.text);
-                  errorCheck();
-                  if (!(_showSellValidationError || _showBuyValidationError)) {
-                    calculate();
-                  }
-                },
-                child: Text(
-                  'Calculate',
-                  style: TextStyle(color: Colors.white, fontSize: width / 25),
-                ),
-              ),
-              SizedBox(
-                height: height * 0.03,
-              ),
-              Visibility(
-                visible: isCalActive,
-                child: Container(
-                  width: width * 0.7,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        text('P&L: ₹${calculation.totalProfit}'),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        text('${calculation.profitper}%'),
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0x40000000),
-                          blurRadius: 4,
-                          offset: Offset(0, 0))
+
+      //Wrapping with gesture detector to hide keyboard
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          /*This method here will hide the soft keyboard.*/
+          FocusScope.of(context).unfocus();
+        },
+        child: SafeArea(
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      textField('Buy Amount', _buyController),
+                      SizedBox(
+                        width: width * 0.05,
+                      ),
+                      textField('Sell Amount', _sellController),
                     ],
                   ),
                 ),
-              )
-            ],
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    primary: Color(0XFF0043b4),
+                  ),
+                  onPressed: () {
+                    //Implement null check both input fields
+                    print(_buyController.text);
+                    errorCheck();
+                    if (!(_showSellValidationError ||
+                        _showBuyValidationError)) {
+                      calculate();
+                    }
+                  },
+                  child: Text(
+                    'Calculate',
+                    style: TextStyle(color: Colors.white, fontSize: width / 25),
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.03,
+                ),
+                Visibility(
+                  visible: isCalActive,
+                  child: Container(
+                    width: width * 0.7,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          text('P&L: ₹${calculation.totalProfit}'),
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+                          text('${calculation.profitper}%'),
+                        ],
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color(0x40000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 0))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
